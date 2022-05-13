7cb211c42f17ec2f610348a219802e3f7fd95a22
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-13 05:47:02 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tar1971/xtraevent/main/installer1.sh -O - | /bin/sh
+##
+###########################################
+###########################################
+#!/bin/sh
+echo
+
+wget http://myflow.boxpirates.dynvpn.de/myflow-installer_dreamos.sh -O - | /bin/sh
